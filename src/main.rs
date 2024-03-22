3ac953e69456e27b4e2b2aa4d0bc97c06deedcb0
Cuprum Wangzuo <cuprumwz@foxmail.com>
2024-03-22 22:45:14 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -42,57 +42,23 @@ async fn main(spawner: Spawner) {
     }
 }
 
-
-// #![no_std]
-// #![no_main]
-// 
-// extern crate alloc;
-// use core::mem::MaybeUninit;
-// use esp32c3_hal::{clock::ClockControl, peripherals::Peripherals, prelude::*, Delay};
+// use embassy_net::tcp::TcpSocket;
+// use embassy_net::{
+//     Config, IpListenEndpoint, Ipv4Address, Ipv4Cidr, Stack, StackResources, StaticConfigV4,
+// };
+// use esp_hal as hal;
+//
+// use embassy_executor::Spawner;
+// use embassy_time::{Duration, Timer};
 // use esp_backtrace as _;
-// use esp_println::println;
-// 
+// use esp_println::{print, println};
+// use esp_wifi::wifi::{AccessPointConfiguration, ClientConfiguration, Configuration};
+// use esp_wifi::wifi::{
+//     WifiApDevice, WifiController, WifiDevice, WifiEvent, WifiDeviceMode, WifiState,
+// };
 // use esp_wifi::{initialize, EspWifiInitFor};
-// 
-// use esp32c3_hal::{systimer::SystemTimer, Rng};
-// #[global_allocator]
-// static ALLOCATOR: esp_alloc::EspHeap = esp_alloc::EspHeap::empty();
-// 
-// fn init_heap() {
-//     const HEAP_SIZE: usize = 32 * 1024;
-//     static mut HEAP: MaybeUninit<[u8; HEAP_SIZE]> = MaybeUninit::uninit();
-// 
-//     unsafe {
-//         ALLOCATOR.init(HEAP.as_mut_ptr() as *mut u8, HEAP_SIZE);
-//     }
-// }
-// #[entry]
-// fn main() -> ! {
-//     init_heap();
-//     let peripherals = Peripherals::take();
-//     let system = peripherals.SYSTEM.split();
-// 
-//     let clocks = ClockControl::max(system.clock_control).freeze();
-//     let mut delay = Delay::new(&clocks);
-// 
-//     // setup logger
-//     // To change the log_level change the env section in .cargo/config.toml
-//     // or remove it and set ESP_LOGLEVEL manually before running cargo run
-//     // this requires a clean rebuild because of https://github.com/rust-lang/cargo/issues/10358
-//     // esp_println::logger::init_logger_from_env();
-//     log::info!("Logger is setup");
-//     println!("Hello world!");
-//     let timer = SystemTimer::new(peripherals.SYSTIMER).alarm0;
-//     let _init = initialize(
-//         EspWifiInitFor::Wifi,
-//         timer,
-//         Rng::new(peripherals.RNG),
-//         system.radio_clock_control,
-//         &clocks,
-//     )
-//     .unwrap();
-//     loop {
-//         println!("Loop...");
-//         delay.delay_ms(500u32);
-//     }
-// }
+//
+// use hal::clock::ClockControl;
+// use hal::Rng;
+// use hal::{embassy, peripherals::Peripherals, prelude::*, timer::TimerGroup};
+// use static_cell::make_static;
